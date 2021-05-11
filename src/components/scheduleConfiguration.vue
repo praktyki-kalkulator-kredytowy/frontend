bce5b07d281c5291600226be82937bc7ddb25a6e
sadursky <sadur3333@gmail.com>
2021-05-11 10:15:39 +0200

    feat(validation): added form validation

--- src/components/scheduleConfiguration.vue
+++ src/components/scheduleConfiguration.vue
@@ -1,15 +1,13 @@
 <template>
 
     <div class="form">
-        <div class="errorAlert" v-if="errors.length">
-            <h1>Proszę o poprawę następujących błędów:</h1>
-            <ul>
-                <li :key="error.index" v-for="error in errors"> {{ error }} </li>
-            </ul>
-        </div>
+       
         <div class="form-row">
             <label>Kapitał: </label>
             <input v-model="capital" class="capital-input" type="number" placeholder="kapitał">
+            <p v-if="v$.capital.$error">
+                {{ v$.capital.$errors[0].$message }}
+            </p>
         </div>
 
         <div class="form-row">
@@ -20,37 +18,55 @@
 
                 
             </select>
+            <p v-if="v$.installmentType.$error">
+                {{ v$.installmentType.$errors[0].$message }}
+            </p>
         </div>
 
         <div class="form-row">
             <label>Liczba rat: </label>
             <input v-model="installmentAmount" type="number" placeholder="liczba rat">
+            <p v-if="v$.installmentAmount.$error">
+                {{ v$.installmentAmount.$errors[0].$message }}
+            </p>
         </div>
 
         <div class="form-row">
             <label>Oprocentowanie: </label>
             <input v-model="interestRate" type="number" placeholder="oprocentowanie">
+            <p v-if="v$.interestRate.$error">
+                {{ v$.interestRate.$errors[0].$message }}
+            </p>
         </div>
 
         <div class="form-row">
             <label>Data wypłaty: </label>
             <input v-model="withdrawalDate" type="date" placeholder="data wypłaty">
+            <p v-if="v$.withdrawalDate.$error">
+                {{ v$.withdrawalDate.$errors[0].$message }}
+            </p>
         </div>
 
         <div class="form-row">
             <label>Prowizja </label>
             <input v-model="commission" type="number" placeholder="prowizja">
+            <p v-if="v$.commission.$error">
+                {{ v$.commission.$errors[0].$message }}
+            </p>
         </div>
 
         <div class="form-row">
             <label>Ubezpieczenie?</label>
-            <select v-model="hasInsurance">
-                <option value='yes'>Tak</option>
-                <option value='no'>Nie</option>   
+            <select v-model="insurance">
+                <option value=true>Tak</option>
+                <option value=false>Nie</option>   
             </select>
+            <p v-if="v$.insurance.$error">
+                {{ v$.insurance.$errors[0].$message }}
+            </p>
         </div>
         
-        <div class="form-row" v-show="hasInsurance ==='yes' ">
+        <div class="form-row" v-show="insurance">
             <label>Wiek </label>
              <select v-model="ageGroup">
                 <option value="under30">18-30</option>
@@ -58,70 +74,65 @@
                 <option value="under65">51-65</option>
                 <option value="over65">66+</option>   
         </select>
+        <p v-if="v$.ageGroup.$error">
+                {{ v$.ageGroup.$errors[0].$message }}
+            </p>
         </div>
-        <button @click="checkForm(), submit()">Wylicz</button>
-    
+        <button @click="submit()">Wylicz</button>
+   
 </div>
 
 </template>
 
 <script>
+import useValidate from '@vuelidate/core'
+import { required, minValue, maxValue, helpers } from '@vuelidate/validators'
 
+const notEmpty = 'Pole nie może być puste'
 export default {
     data() {
         return {
-            errors: [],
+            v$: useValidate(),
             capital: null,
             installmentType: null,
             installmentAmount: null,
             interestRate: null,
             withdrawalDate: null,
             commission: null,
-            hasInsurance: null,
+            insurance: false,
             ageGroup: null
         }
     },
-
+    validations() {
+        return {
+            capital: {
+                 required: helpers.withMessage(notEmpty, required) },
+            installmentType: { 
+                required: helpers.withMessage(notEmpty, required) },
+            installmentAmount: { 
+                required: helpers.withMessage(notEmpty, required) },
+            interestRate: { 
+                required: helpers.withMessage(notEmpty, required) },
+            withdrawalDate: { 
+                required: helpers.withMessage(notEmpty, required) },
+            commission: { 
+                required: helpers.withMessage(notEmpty, required) , 
+                minValue: helpers.withMessage('Prowizja musi być większa od zera.', minValue(0)),
+                maxValue: helpers.withMessage('Prowizja może wynosić maksymalnie 20%', maxValue(20)) },
+            insurance: { 
+                required: helpers.withMessage(notEmpty, required) },
+            ageGroup: { 
+                required: helpers.withMessage(notEmpty, required) },
+        }
+    },
     props: {
         onSubmit: Function
     },
     methods: {
-        checkForm() {
-            if (this.capital && this.installmentType && this.installmentAmount 
-            && this.interestRate && this.withdrawalDate && this.commission
-            &&this.hasInsurance && this.ageGroup ) return true
-
-            this.errors = [];
-
-            if (!this.capital) {
-                this.errors.push("Nieprawidłowa kwota.")
-            }
-            if (!this.installmentType) {
-                this.errors.push("Określ typ rat.")
-            }
-            if (!this.installmentAmount) {
-                this.errors.push("Nieprawidłowa liczba rat.")
-            }
-            if (!this.interestRate) {
-                this.errors.push("Nieprawidłowe oprocentowanie.")
-            }
-            if (!this.withdrawalDate) {
-                this.errors.push("Wybierz datę wypłaty.")
-            }
-            if (!this.commission || this.commission < 0 || this.commission > 20) {
-                this.errors.push("Nieprawidłowa prowizja(0-20%).")
-            }
-            if (this.hasInsurance == 'yes') {
-                if (!this.ageGroup) 
-                    this.errors.push("Określ grupę wiekową")
-            }
-            else if (this.hasInsurance != 'no') 
-                this.errors.push("Określ czy wymagane jest ubezpieczenie.")
-
-
-        },
+       
         submit() { 
-            if(this.errors.length>0) return
+            this.v$.$validate()
+            if (!this.v$.$error) {
             if(this.onSubmit != undefined) {
                 this.onSubmit({
                     capital: this.capital,
@@ -132,11 +143,13 @@ export default {
                 })
             }
         }
-        
+        else  {
+            alert('Nieprawidłowe dane')
+        }
 
     }
 }
-
+}
 </script>
 
 <style scoped>
@@ -171,7 +184,10 @@ export default {
         border: 2px solid #758078;
         border-radius: 7px;
     }
-
+    .form-row p {
+        color: red;
+        display: block;
+    }
 
     button {
         cursor: pointer;
@@ -189,7 +205,6 @@ export default {
         margin: 20px;
         padding: 10px;
     }
-    .errorAlert h1 {
-        color: rgb(200, 0, 0)
-    }
+    
+    
 </style>
