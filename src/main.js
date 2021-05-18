fe293bfe140e248407509cdb1dcab6376be0d868
sadursky <sadur3333@gmail.com>
2021-05-18 08:20:40 +0200

    feat(v-calendar): library added

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import VCalendar from 'v-calendar'
 
 createApp(App)
     .use(router)
+    .use(VCalendar, {})
     .mount('#app')
 
