81f9497f2c35c190e47294919ef6f65d2cda7065
sadursky <sadur3333@gmail.com>
2021-05-19 19:53:32 +0200

    feat(importJSON): import schedule data from json file, added backend error popup

--- src/views/Harmonogram.vue
+++ src/views/Harmonogram.vue
@@ -1,7 +1,20 @@
 <template> 
 <div>
-  <schedule-configuration :onSubmit="fetchSchedule"/>
+  <section v-if="errors.length">
+    <p>Wystąpił problem z aplikacją. Proszę skontaktuj się z administratorem.</p>
+  </section>
 
+  <section v-else>
+    <div v-if="loading">Proszę czekać...</div>
+  </section>
+  <schedule-configuration :onSubmit="fetchSchedule" :loading="loading" />
+  <br>
+    
+  <button @click="toggleShowImport">Importuj z pliku JSON</button>
+  <div v-show="showImport">
+            <button @click="importJSON()">Zatwierdź</button>
+            <input type="file" id="selectJSON" value="">
+  </div>
 
   <br/>
   <router-link to="/">Przejdź do strony głównej</router-link>
@@ -10,7 +23,7 @@
   <div class="scheduleContainer">
     <schedule :installments="installments" />
   </div>
-  
+
 
 </div>
 </template>
@@ -34,21 +47,42 @@ export default {
         installmentList: [],
         aprc: 0,
         insurancePremiumList: []
-      }
+      },
+      showImport: false,
+      errors: [],
+      loading: false
     }
   
   },
   methods: 
   {
      async fetchSchedule(data) {
+       this.loading = true
         let response =  await axios.post(`http://localhost:4200/api/v1/schedule`, JSON.stringify(data), { 
           headers:{"Content-type" : "application/json"}
-          });
+          })
+          .catch(error => this.errors.push(error.message))
+          .finally(() => this.loading = false)
 
           this.installments = response.data;
     },
-    
-    
+    importJSON() {
+        const files = document.getElementById('selectJSON').files;
+        if (files.length <= 0) {
+          return false;
+        }
+
+        const fr = new FileReader();
+
+        fr.onload = e => {
+          const result = JSON.parse(e.target.result);
+          this.installments = result
+        }
+        fr.readAsText(files.item(0));
+        },
+        toggleShowImport() {
+            this.showImport = !this.showImport
+        }
   }
   
 
