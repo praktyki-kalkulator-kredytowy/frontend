8aed062f89a8c4ece3f61ca1630a07bced0e7264
sadursky <sadur3333@gmail.com>
2021-05-21 08:46:39 +0200

    feat: inputs refresh after deleting configuration

--- src/views/UserConfig.vue
+++ src/views/UserConfig.vue
@@ -4,42 +4,50 @@
       <label>Minimalne oprocentowanie(%):</label>
       <input type="number" v-model="minInterestRate" required>
       <button @click="setConfiguration('MIN_INTEREST_RATE', minInterestRate/100, 'DEFAULT')">Edytuj</button>
-      <button @click="deleteConfiguration('MIN_INTEREST_RATE', 'DEFAULT'); minInterestRate = null">Usuń</button>
+      <button @click="deleteConfiguration('MIN_INTEREST_RATE', 'DEFAULT'); minInterestRate = null;
+                       getConfiguration('minInterestRate', 'MIN_INTEREST_RATE', true)">Usuń</button>
     <br>
       <label>Maksymalne oprocentowanie(%):</label>
       <input type="number" v-model="maxInterestRate" required>
       <button @click="setConfiguration('MAX_INTEREST_RATE', maxInterestRate/100, 'DEFAULT')">Edytuj</button>
-      <button @click="deleteConfiguration('MAX_INTEREST_RATE', 'DEFAULT'); maxInterestRate = null">Usuń</button>
+      <button @click="deleteConfiguration('MAX_INTEREST_RATE', 'DEFAULT'); maxInterestRate = null;
+                      getConfiguration('maxInterestRate', 'MAX_INTEREST_RATE', true)">Usuń</button>
     <br>
       <label>Minimalna kwota prowizji(PLN):</label>
       <input type="number" v-model="minCommissionAmount" required>
       <button @click="setConfiguration('MIN_COMMISSION_AMOUNT', minCommissionAmount, 'DEFAULT')">Edytuj</button>
-      <button @click="deleteConfiguration('MIN_COMMISSION_AMOUNT', 'DEFAULT'); minCommissionAmount = null">Usuń</button>
+      <button @click="deleteConfiguration('MIN_COMMISSION_AMOUNT', 'DEFAULT'); minCommissionAmount = null;
+                      getConfiguration('minCommissionAmount', 'MIN_COMMISSION_AMOUNT', false)">Usuń</button>
     <br>
       <label>Minimalna wartość prowizji(%):</label>
       <input type="number" v-model="minCommissionRate" required>
       <button @click="setConfiguration('MIN_COMMISSION_RATE', minCommissionRate/100, 'DEFAULT')">Edytuj</button>
-      <button @click="deleteConfiguration('MIN_COMMISSION_RATE', 'DEFAULT'); minCommissionRate = null">Usuń</button>
+      <button @click="deleteConfiguration('MIN_COMMISSION_RATE', 'DEFAULT'); minCommissionRate = null;
+                      getConfiguration('minCommissionRate', 'MIN_COMMISSION_RATE', true)">Usuń</button>
     <br>
       <label>Maksymalna wartość prowizji(%):</label>
       <input type="number" v-model="maxCommissionRate" required>
       <button @click="setConfiguration('MAX_COMMISSION_RATE', maxCommissionRate/100, 'DEFAULT')">Edytuj</button>
-      <button @click="deleteConfiguration('MAX_COMMISSION_RATE', 'DEFAULT'); maxCommissionRate = null">Usuń</button>
+      <button @click="deleteConfiguration('MAX_COMMISSION_RATE', 'DEFAULT'); maxCommissionRate = null
+                      getConfiguration('maxCommissionRate', 'MAX_COMMISSION_RATE', true)">Usuń</button>
     <br>
       <label>Domyślna wartość prowizji(%):</label>
       <input type="number" v-model="defaultCommissionRate" required>
       <button @click="setConfiguration('DEFAULT_COMMISSION_RATE', defaultCommissionRate/100, 'DEFAULT')">Edytuj</button>
-      <button @click="deleteConfiguration('DEFAULT_COMMISSION_RATE', 'DEFAULT'); defaultCommissionRate = null">Usuń</button>
+      <button @click="deleteConfiguration('DEFAULT_COMMISSION_RATE', 'DEFAULT'); defaultCommissionRate = null
+                      getConfiguration('defaultCommissionRate', 'DEFAULT_COMMISSION_RATE', true)">Usuń</button>
     <br>
       <label>Minimalna kwota składki ubezpieczeniowej(PLN):</label>
       <input type="number" v-model="minInsurancePremium" required>
       <button @click="setConfiguration('MIN_PREMIUM_VALUE', minInsurancePremium, 'DEFAULT')">Edytuj</button>
-      <button @click="deleteConfiguration('MIN_PREMIUM_VALUE', 'DEFAULT'); minInsurancePremium = null">Usuń</button>
+      <button @click="deleteConfiguration('MIN_PREMIUM_VALUE', 'DEFAULT'); minInsurancePremium = null
+                      getConfiguration('minInsurancePremium', 'MIN_PREMIUM_VALUE', false)">Usuń</button>
     <br>
       <label>Częstotliwość płatności składki ubezpieczeniowej(mies.):</label>
       <input type="number" v-model="PaymentFrequency" required>
       <button @click="setConfiguration('MONTH_FRAME', PaymentFrequency, 'DEFAULT')">Edytuj</button>
-      <button @click="deleteConfiguration('MONTH_FRAME', 'DEFAULT'); PaymentFrequency = null">Usuń</button>
+      <button @click="deleteConfiguration('MONTH_FRAME', 'DEFAULT'); PaymentFrequency = null;
+                      getConfiguration('PaymentFrequency', 'MONTH_FRAME', false)">Usuń</button>
     <br><br>
       <button @click="addBracket">Dodaj przedział wiekowy</button>
     <br>
@@ -89,8 +97,8 @@ export default {
   },
   methods: {
     async getConfiguration(_data, key, isPercentage) {
-       await axios.get(`http://localhost:4200/api/v1/schedule/configuration/group?groupKey=DEFAULT&key=${key}`)
-      .then(response => _data = response.data * (isPercentage ? 100 : 1))
+       await axios.get(`http://localhost:4200/api/v1/schedule/configuration?groupKey=DEFAULT&key=${key}`)
+      .then(response => this[_data] = response.data.value * (isPercentage ? 100 : 1))
     },
     async getAgeBrackets() {
         await axios.get(`http://localhost:4200/api/v1/schedule/configuration/group?groupKey=INSURANCE_GROUPS`)
@@ -124,7 +132,7 @@ export default {
         key: key,
         groupKey: groupKey
       }
-      let response = axios.delete(`http://localhost:4200/api/v1/schedule/configuration`, { data: data })
+      let response = await axios.delete(`http://localhost:4200/api/v1/schedule/configuration`, { data: data })
       response.status == 200 ? alert('Udało się usunąć konfigurację.') : alert('Wystąpił błąd.')
     },
     addBracket() {
@@ -139,14 +147,14 @@ export default {
   
   },
   mounted() {
-    this.getConfiguration(this.minInsurancePremium, "MIN_PREMIUM_VALUE", false),
-    this.getConfiguration(this.minCommissionAmount, "MIN_COMMISSION_AMOUNT", false),
-    this.getConfiguration(this.minInterestRate, "MIN_INTEREST_RATE", true),
-    this.getConfiguration(this.maxInterestRate, "MAX_INTEREST_RATE", true),
-    this.getConfiguration(this.minCommissionRate, "MIN_COMMISSION_RATE", true),
-    this.getConfiguration(this.maxCommissionRate, "MAX_COMMISSION_RATE", true),
-    this.getConfiguration(this.defaultCommissionRate, "DEFAULT_COMMISSION_RATE", true),
-    this.getConfiguration(this.PaymentFrequency, "MONTH_FRAME", false),
+    this.getConfiguration('minInsurancePremium', "MIN_PREMIUM_VALUE", false),
+    this.getConfiguration('minCommissionAmount', "MIN_COMMISSION_AMOUNT", false),
+    this.getConfiguration('minInterestRate', "MIN_INTEREST_RATE", true),
+    this.getConfiguration('maxInterestRate', "MAX_INTEREST_RATE", true),
+    this.getConfiguration('minCommissionRate', "MIN_COMMISSION_RATE", true),
+    this.getConfiguration('maxCommissionRate', "MAX_COMMISSION_RATE", true),
+    this.getConfiguration('defaultCommissionRate', "DEFAULT_COMMISSION_RATE", true),
+    this.getConfiguration('PaymentFrequency', "MONTH_FRAME", false),
     this.getAgeBrackets()
   }
 }
