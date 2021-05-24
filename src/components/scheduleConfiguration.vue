5f7f6bc0625fc63cc0ab358670a58d243cac1a73
xsw2-2wsx <jan1undrych@gmail.com>
2021-05-24 10:10:09 +0200

    fix(JsonImport): readd showImport

--- src/components/scheduleConfiguration.vue
+++ src/components/scheduleConfiguration.vue
@@ -125,6 +125,7 @@ export default {
             commissionRate: null,
             insurance: false,
             age: null,
+            showImport: false,
             conf: {}
             }
     },
