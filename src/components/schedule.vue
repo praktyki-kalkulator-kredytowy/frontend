f1783a0137eddf03f7da15e5db243b9e66f35225
witas1 <mruczekiklaczek@gmail.com>
2021-05-24 20:56:02 +0200

    fix: deleted example table

--- src/components/schedule.vue
+++ src/components/schedule.vue
@@ -79,23 +79,7 @@
     <button @click="downloadPDF">Pobierz PDF</button>
     
     </div>
-<table style="width:100%">
-  <tr>
-    <th>Firstname</th>
-    <th>Lastname</th>
-    <th>Age</th>
-  </tr>
-  <tr>
-    <td>Jill</td>
-    <td>Smith</td>
-    <td>50</td>
-  </tr>
-  <tr>
-    <td>Eve</td>
-    <td>Jackson</td>
-    <td>94</td>
-  </tr>
-</table>
+
 </template>
 
 <script>
