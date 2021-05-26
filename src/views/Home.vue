b1e54ee96c01c65a2fbb96b99f543ac76a79bd8e
sadursky <sadur3333@gmail.com>
2021-05-26 10:57:34 +0200

    feat(audit): added link

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,6 +4,8 @@
   <router-link to="/harmonogram">Przejdź do kalkulatora kredytowego</router-link>
   <br>
   <router-link to="/config">Przejdź do edytora konfiguracji</router-link>
+  <br>
+  <router-link to="/audit">Przejdź do danych audytowych</router-link>
 </template>
 
 <script>
