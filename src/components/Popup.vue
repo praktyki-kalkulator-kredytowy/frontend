343b9ed5ef296ddbfe343706d06c1127b73e74e6
sadursky <sadur3333@gmail.com>
2021-05-27 13:23:42 +0200

    feat(Popup): created reusable component

---
+++ src/components/Popup.vue
@@ -0,0 +1,86 @@
+<template>
+    <div class="modal-mask">
+      <div class="modal-wrapper">
+        <div class="modal-container">
+
+          <div class="modal-header">
+            <slot name="header">
+              default header
+            </slot>
+          </div>  
+
+          <div class="modal-body">
+            <slot name="body">
+              default body
+            </slot>
+          </div>
+
+          <div class="modal-footer">
+            <slot name="footer">
+            
+              
+            </slot>
+            
+          </div>
+          <button class="modal-default-button" @click="$emit('close')">
+                OK
+            </button>
+        </div>
+      </div>
+    </div> 
+</template>
+
+<script>
+
+
+export default {
+    
+}
+</script>
+
+<style scoped>
+.modal-mask {
+  position: fixed;
+  z-index: 9998;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: table;
+}
+
+.modal-wrapper {
+  display: table-cell;
+  vertical-align: middle;
+}
+
+.modal-container {
+  width: 300px;
+  margin: auto;
+  padding: 20px 30px 60px;
+  background-color: #fff;
+  border-radius: 2px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
+  font-family: Helvetica, Arial, sans-serif;
+}
+
+.modal-header h3 {
+  margin-top: 0;
+  color: #42b983;
+}
+
+.modal-body {
+  margin: 20px 0;
+}
+.modal-footer{
+    min-height: 20px;
+    padding: 10px;
+}
+.modal-default-button {
+  float: right;
+  margin: 20px;
+  width: 60px;
+
+}
+</style>
